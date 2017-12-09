f9836c5be7e4218fbbba39a00b29cb649a133594
Hobbs-Katherine <32024730+Hobbs-Katherine@users.noreply.github.com>
2017-12-08 19:03:13 -0700

    add psudo links, timeline, and Christmas message

--- FamilyTree/home.js
+++ FamilyTree/home.js
@@ -17,7 +17,6 @@ function DeceasedPerson(name, image, birthdate, birthplace, deathdate, deathplac
     this.deathplace = deathplace;
 };
 
-
 function getPeople() {
     var siblingAncestors =  [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
    
@@ -41,39 +40,39 @@ function getPeople() {
     personList["indv-12"] = new LivingPerson("Dorothy Christiansen", "images/12.jpg", [19, 20, 33, 34, 35, 36], "videos/dorothy.mp4");
     
     //gen-3
-    personList["indv-13"] = new DeceasedPerson("Mac Kay Hunter", "images/13.jpg", "6 July 1906", "Mount Glen, Union, Oregon, United States", "11 April 1997", "Boise, Ada, Idaho, United States", [21, 22]);
-    personList["indv-14"] = new DeceasedPerson("Edith Roesberry", "images/14.jpg", "24 October 1912", "Miami Mariposa, California, United States", "29 May 1999", "Kuna, Ada, Idaho, United States", [23, 24]);
-    personList["indv-15"] = new DeceasedPerson("Albert Mackley", "images/15.jpg", "9 May 1898", "Spanish Fork, Utah, Utah, United States", "29 August 1992", "Saint George, Washington, Utah, United States", [25, 26]);
-    personList["indv-16"] = new DeceasedPerson("Pearl Dalby", "images/16.jpg", "23 December 1903", "Omaha, Douglas, Nebraska, United States", "20 January 1968", "Saint George, Washington, Utah, United States", [27, 28]);
-    personList["indv-17"] = new DeceasedPerson("Earl Ricks", "images/17.jpg", "21 November 1895", "Salem, Fremont, Idaho, United States", "15 September 1964", "Teton, Madison, Idaho", [29, 30]);
-    personList["indv-18"] = new DeceasedPerson("Theora Valentine", "images/18.jpg", "5 December 1899", "Salem, Fremont, Idaho, United States", "27 February 1985", "Idaho Falls, Bonneville, Idaho, United States", [31, 32]);
-    personList["indv-19"] = new DeceasedPerson("James Christiansen", "images/19.jpg", "12 September 1908", "Hyrum Cache, Utah, United States", "12 July 1961", "Idaho Falls, Bonneville, Idaho, United States", [33, 34]);
-    personList["indv-20"] = new DeceasedPerson("Rosetta Williams", "images/20.jpg", "1 August 1920", "Lava Hot Springs, Bannock, Idaho, United States", "4 June 2005", "Rexburg, Madison, Idaho, United States", [35, 36]);
+    personList["indv-13"] = new DeceasedPerson("Mac Kay Hunter", "images/13.jpg", "July 6, 1906", "Mount Glen, Union, Oregon, United States", "April 11, 1997", "Boise, Ada, Idaho, United States", [21, 22]);
+    personList["indv-14"] = new DeceasedPerson("Edith Roesberry", "images/14.jpg", "October 24, 1912", "Miami Mariposa, California, United States", "May 29, 1999", "Kuna, Ada, Idaho, United States", [23, 24]);
+    personList["indv-15"] = new DeceasedPerson("Albert C. Mackley", "images/15.jpg", "May 9, 1898", "Spanish Fork, Utah, Utah, United States", "August 29, 1992", "Saint George, Washington, Utah, United States", [25, 26]);
+    personList["indv-16"] = new DeceasedPerson("Pearl Dalby", "images/16.jpg", " December 23, 1903", "Omaha, Douglas, Nebraska, United States", "January 20, 1968", "Saint George, Washington, Utah, United States", [27, 28]);
+    personList["indv-17"] = new DeceasedPerson("Earl Ricks", "images/17.jpg", " November 21, 1895", "Salem, Fremont, Idaho, United States", "September 15, 1964", "Teton, Madison, Idaho", [29, 30]);
+    personList["indv-18"] = new DeceasedPerson("Theora Valentine", "images/18.jpg", "December 5, 1899", "Salem, Fremont, Idaho, United States", "February 27, 1985", "Idaho Falls, Bonneville, Idaho, United States", [31, 32]);
+    personList["indv-19"] = new DeceasedPerson("James Christiansen", "images/19.jpg", "September 12, 1908", "Hyrum Cache, Utah, United States", "July 12, 1961", "Idaho Falls, Bonneville, Idaho, United States", [33, 34]);
+    personList["indv-20"] = new DeceasedPerson("Rosetta Williams", "images/20.jpg", "August 1, 1920", "Lava Hot Springs, Bannock, Idaho, United States", "June 4, 2005", "Rexburg, Madison, Idaho, United States", [35, 36]);
    
     //gen-4
-    personList["indv-21"] = new DeceasedPerson("John Hunter", "images/21.jpg", "26 March 1869", "Grantsville, Toole, Utah, United States", "15 June 1959", "Emmett, Gem, Idaho, United States", []);
-    personList["indv-22"] = new DeceasedPerson("Charlotte Hale", "images/22.jpg", "5 December 1872", "Grantsville, Toole, Utah, United States", "14 April 1943", "Emmett, Gem, Idaho, United States", []);
+    personList["indv-21"] = new DeceasedPerson("John Hunter", "images/21.jpg", "March 26, 1869", "Grantsville, Toole, Utah, United States", "June 15, 1959", "Emmett, Gem, Idaho, United States", []);
+    personList["indv-22"] = new DeceasedPerson("Charlotte Hale", "images/22.jpg", "December 5, 1872", "Grantsville, Toole, Utah, United States", "April 14, 1943", "Emmett, Gem, Idaho, United States", []);
     
-    personList["indv-23"] = new DeceasedPerson("Seth Roesberry", "images/23.jpg", "23 July 1883", "Pima, Grahm, Arizona, United States", "2 January 1949", "Provo, Utah, Utah, United States", []);
-    personList["indv-24"] = new DeceasedPerson("Mary Rogers", "images/24.jpg", "15 May 1885", "Luna Valley, Socorro, New Mexico, United States", "15 February 1971", "Saint George, Washington, Utah, United States", []);
+    personList["indv-23"] = new DeceasedPerson("Seth Roesberry", "images/23.jpg", "July  23, 1883", "Pima, Grahm, Arizona, United States", "January 2, 1949", "Provo, Utah, Utah, United States", []);
+    personList["indv-24"] = new DeceasedPerson("Mary Rogers", "images/24.jpg", "May 15, 1885", "Luna Valley, Socorro, New Mexico, United States", "February 15, 1971", "Saint George, Washington, Utah, United States", []);
     
-    personList["indv-25"] = new DeceasedPerson("Albert Mackley", "images/25.jpg", "25 August 1873", "Norwich, Norfolk, England", "31 August 1967", "St. George, Washington, Utah, United States", []);
-    personList["indv-26"] = new DeceasedPerson("Catherine Braithwaite", "images/26.jpg", "25 May 1879", "Manti, Sanpete, Utah, United States", "30 April 1949", "Mapleton, Utah, Utah, United States", []);
+    personList["indv-25"] = new DeceasedPerson("Albert J. Mackley", "images/25.jpg", "August 25, 1873", "Norwich, Norfolk, England", "August 31, 1967", "St. George, Washington, Utah, United States", []);
+    personList["indv-26"] = new DeceasedPerson("Catherine Braithwaite", "images/26.jpg", "May 25, 1879", "Manti, Sanpete, Utah, United States", "April 30, 1949", "Mapleton, Utah, Utah, United States", []);
     
-    personList["indv-27"] = new DeceasedPerson("William Dalby", "images/27.jpg", "17 September 1872", "Kansas City, Wyandotte, Kansas, United States", "26 April 1924", "Gooding, Gooding, Idaho, United States", []);
-    personList["indv-28"] = new DeceasedPerson("Catherine Bronson", "images/28.jpg", "26 October 1876", "Luray, Russell, Kansas, United States", "18 December 1908", "Topeka, Shawnee, Kansas, United States", []);
+    personList["indv-27"] = new DeceasedPerson("William Dalby", "images/27.jpg", "September 17, 1872", "Kansas City, Wyandotte, Kansas, United States", "April 26, 1924", "Gooding, Gooding, Idaho, United States", []);
+    personList["indv-28"] = new DeceasedPerson("Catherine Bronson", "images/28.jpg", "October 26, 1876", "Luray, Russell, Kansas, United States", "December 18, 1908", "Topeka, Shawnee, Kansas, United States", []);
     
-     personList["indv-29"] = new DeceasedPerson("John Ricks", "images/29.jpg", "10 May 1868", "Logan, Cache, Utah, United States", "4 May 1920", "Teton, Idaho, United States", []);
-    personList["indv-30"] = new DeceasedPerson("Julianna Hansen", "images/30.jpg", "12 June 1871", "Trelose, Skelby, Soro, Denmark", "12 January 1950", "Rexburg, Madison, Idaho, United States", []);
+     personList["indv-29"] = new DeceasedPerson("John Ricks", "images/29.jpg", "May 10, 1868", "Logan, Cache, Utah, United States", "May 4, 1920", "Teton, Idaho, United States", []);
+    personList["indv-30"] = new DeceasedPerson("Julianna Hansen", "images/30.jpg", "June 12, 1871", "Trelose, Skelby, Soro, Denmark", "January 12, 1950", "Rexburg, Madison, Idaho, United States", []);
     
-    personList["indv-31"] = new DeceasedPerson("Charles Valentine", "images/31.jpg", "3 March 1873", "Calls Fort, Box Elder, Utah, United States", "16 April 1933", "Salem, Fremont, Idaho, United States", []);
-    personList["indv-32"] = new DeceasedPerson("Minerva Dille", "images/32.jpg", "11 September 1872", "Hyrum, Cache, Utah, United States", "29 December 1913", "Rexburg, Madison, Idaho, United States", []);
+    personList["indv-31"] = new DeceasedPerson("Charles Valentine", "images/31.jpg", "March 3, 1873", "Calls Fort, Box Elder, Utah, United States", "April 16, 1933", "Salem, Fremont, Idaho, United States", []);
+    personList["indv-32"] = new DeceasedPerson("Minerva Dille", "images/32.jpg", "September 11, 1872", "Hyrum, Cache, Utah, United States", "December 26, 1913", "Rexburg, Madison, Idaho, United States", []);
     
-    personList["indv-33"] = new DeceasedPerson("Joseph Christiansen", "images/33.jpg", "11 November 1882", "Hyrum, Cache, Utah, United States", "6 November 1918", "Menan, Jefferson, Idaho", []);
-    personList["indv-34"] = new DeceasedPerson("Sarah Baron", "images/34.jpg", "31 October 1884", "Brigham City, Box, Elder, Utah, United States", "11 March 1962", "Soda Springs, Caribou, Idaho, United States", []);
+    personList["indv-33"] = new DeceasedPerson("Joseph Christiansen", "images/33.jpg", "November 11, 1882", "Hyrum, Cache, Utah, United States", "November 6, 1918", "Menan, Jefferson, Idaho", []);
+    personList["indv-34"] = new DeceasedPerson("Sarah Baron", "images/34.jpg", "October 31, 1884", "Brigham City, Box, Elder, Utah, United States", "March 11, 1962", "Soda Springs, Caribou, Idaho, United States", []);
     
-    personList["indv-35"] = new DeceasedPerson("Edward Williams", "images/35.jpg", "13 August 1876", "Council Bluffs, Pottawattamie, Iowa, United States", "22 December 1962", "Downey, Bannock, Idaho, United States", []);
-    personList["indv-36"] = new DeceasedPerson("Hazel Reese", "images/36.jpg", "1 May 1895", "Dempsey, Bannock, Idaho, United States", "17 July 1964", "Pocatello, Bannock, Idaho, United States", []);
+    personList["indv-35"] = new DeceasedPerson("Edward Williams", "images/35.jpg", "August 13, 1876", "Council Bluffs, Pottawattamie, Iowa, United States", "December 22, 1962", "Downey, Bannock, Idaho, United States", []);
+    personList["indv-36"] = new DeceasedPerson("Hazel Reese", "images/36.jpg", "May 1, 1895", "Dempsey, Bannock, Idaho, United States", "July 17, 1964", "Pocatello, Bannock, Idaho, United States", []);
     return personList;
 }
 
@@ -138,4 +137,141 @@ function displayLivingModal(personId) {
     document.getElementById("video").load();
 }
 
+function loadChart() {
+    document.getElementById("tree").style.display= "none";
+    document.getElementById("chart").style.display= "block";
+    
+    google.charts.load('current', {'packages':['timeline']});
+    google.charts.setOnLoadCallback(drawChart);
+    
+}
 
+function drawChart() {
+    var container = document.getElementById('chart');
+    var chart = new     google.visualization.Timeline(container);
+    var dataTable = new google.visualization.DataTable();
+    
+    dataTable.addColumn({ type: 'string', id: 'Ancestor' });
+    dataTable.addColumn({ type: 'string', id: 'Name' });
+    dataTable.addColumn({ type: 'date', id: 'Start' });
+    dataTable.addColumn({ type: 'date', id: 'End' });
+    
+    var personList = getPeople();
+    var count = 1;
+    for (p in personList) {
+        var id = p.substr(5);
+        if (id > 12) {
+            var person = personList[p];
+            var bday = Date.parse(person.birthdate);
+            var dday = Date.parse(person.deathdate);
+            dataTable.addRow(
+                [ count + "" /*add quotes to cast to string*/, person.name, new Date(bday), new  Date(dday) ]);
+            count++;
+        }
+    }
+    
+    var options = {
+        timeline: { showRowLabels: false },
+        height: 1500
+    };
+    
+    chart.draw(dataTable, options);
+}
+
+function unloadChart() {
+    document.getElementById("tree").style.display= "block";
+    document.getElementById("chart").style.display= "none";
+}
+
+function drawCanvas() {
+    document.getElementById('id03').style.display='block';
+    
+    drawTree();
+    drawLeaf();
+}
+
+function drawTree() {
+    //I made some adaptations to an example found at https://www.spjeff.com/2013/12/05/christmas-tree-html5-js-and-css3/
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+ 
+    //stump
+    ctx.fillStyle = "brown";
+    ctx.fillRect(70, 120, 20, 50);
+    
+    //tree base
+    ctx.fillStyle = "green";
+    ctx.fillRect(60, 50, 40, 90);
+    ctx.fillRect(40, 110, 80, 40);
+    
+    //leaves
+    a = 60;
+    b = 10;
+    for (var i = 0; i < 4; i++) {
+        drawLeaf(-20 + a - (i * 10), b + (i * 30), ctx, "left");
+        drawLeaf(20 + a + (i * 10), b + (i * 30), ctx, "right");
+    }
+ 
+    //star
+    drawStar(ctx, 80, 20, 20, 5, 0.5);
+ 
+    //ornaments 
+    drawCircle(ctx, -5, 35, "red");
+    drawCircle(ctx, 20, 75, "red");
+    drawCircle(ctx, -30, 100, "red");
+    drawHalfCircle1(ctx);
+    drawHalfCircle2(ctx);
+}
+
+function drawLeaf(x, y, ctx, style) {
+    // Right Filled triangle
+    ctx.beginPath();
+    ctx.fillStyle = "green";
+    if (style == "right") {
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + 40, y + 40);
+        ctx.lineTo(x, y + 40);
+    } else {
+        ctx.moveTo(x + 40, y);
+        ctx.lineTo(x, y + 40);
+        ctx.lineTo(x + 40, y + 40);
+    }
+    ctx.fill();
+}
+ 
+function drawStar(ctx, x, y, r, p, m) {
+    ctx.fillStyle = "gold";
+    ctx.beginPath();
+    ctx.translate(x, y);
+    ctx.moveTo(0, 0 - r);
+    for (var i = 0; i < p; i++) {
+        ctx.rotate(Math.PI / p);
+        ctx.lineTo(0, 0 - (r * m));
+        ctx.rotate(Math.PI / p);
+        ctx.lineTo(0, 0 - r);
+    }
+    ctx.fill();
+    ctx.restore();
+}
+
+function drawCircle(ctx, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x,y,5,0,2*Math.PI);
+    ctx.fill();
+    ctx.stroke();
+}
+
+function drawHalfCircle1(ctx) {
+    ctx.strokeStyle = "gold";
+    ctx.beginPath();
+    ctx.arc(-17,25,37,0,0.60*Math.PI);
+    ctx.stroke();
+}
+
+function drawHalfCircle2(ctx) {
+    ctx.strokeStyle = "gold";
+    ctx.beginPath();
+    ctx.arc(6,36,62,0.3*Math.PI,0.8*Math.PI);
+    ctx.stroke();
+}
